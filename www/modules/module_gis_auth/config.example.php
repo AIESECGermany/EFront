bb393bbdcd44ee306bba34e8dd4ace4906e2145e
David Soto Setzke <david.soto@web.de>
2016-08-12 19:33:23 +0200

    added group ID key to example config

--- www/modules/module_gis_auth/config.example.php
+++ www/modules/module_gis_auth/config.example.php
@@ -2,3 +2,4 @@
 $salt = "[Enter salt here]";
 $offices = array('MC-ID', 'LC-ID', 'LC2-ID');
 $membersOnly = true;    //true to allow only TXP people to login or false to allow it everybody.
+$groupID = 1;
